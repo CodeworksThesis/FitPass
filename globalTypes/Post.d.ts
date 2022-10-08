e732580b4f98dbb673cc90a2ad60a28fc896c982
Francis_ldn <fnhh04@gmail.com>
2022-10-08 12:29:19 +0200

    add: global types

---
+++ globalTypes/Post.d.ts
@@ -0,0 +1,13 @@
+export interface Post {
+    id: string,
+    studioName: string,
+    exerciseName: string,
+    desc: string,
+    duration: number, // minutes
+    longitude:number,
+    latitude:number,
+    classDate: Date,
+    exerciseType: string,
+    price: string,
+    postPic:string
+}
