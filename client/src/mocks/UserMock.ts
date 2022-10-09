1d0c425d9860f8fae20d07a18dac07e28f806141
Kristina Aleksejeva <kristi.aleksejeva@gmail.com>
2022-10-09 12:57:16 +0100

    added profile

---
+++ client/src/mocks/UserMock.ts
@@ -0,0 +1,6 @@
+export const UserMock = {
+    id: 'EMILY P.',
+    favourites: [],
+    booked: [],
+    profilePic:'https://i0.wp.com/digital-photography-school.com/wp-content/uploads/2011/07/outdoor-portraits-1.jpg?w=2000&ssl=1',
+}
