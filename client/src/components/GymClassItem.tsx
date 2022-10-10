6bebca885c13bb71118f03dbc24eab8efc5e0de1
Francis_ldn <fnhh04@gmail.com>
2022-10-10 19:04:57 +0200

    format text on gymclass

--- client/src/components/GymClassItem.tsx
+++ client/src/components/GymClassItem.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { formatDate, formatTime } from '../utils/time'
 interface GymClassItemProps {
     exerciseName: string,
     studioName:string,
@@ -20,9 +21,10 @@ export default function GymClassItem ({exerciseName, studioName, classDate, post
         </div>
         <div className="flex flex-col gap-5 bg-[#6F87F5] w-[50%]">
             <div className="flex flex-1 bg-red flex-col text-white pt-[8%] pl-[8%] gap-2">
-                <h2 className="text-[1rem]">{exerciseName}</h2>
-                <p className="text-xs">{studioName}</p>
-                <p className="text-xs">{exerciseType}</p>
+                <h2 className="text-2xl font-bold">{exerciseName}</h2>
+                <p className="text-md">{studioName}</p>
+                <p className="text-md">{formatDate(classDate)}</p>
+                <p className="text-md">{formatTime(classDate)}</p>
             </div>
             {/* based on User Data */}
             <div className="flex justify-between">
