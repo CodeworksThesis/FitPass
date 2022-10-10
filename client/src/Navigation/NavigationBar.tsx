33c64104e8d5ad6f05d5e169a89dda70ee33120f
OliverG <gallantioliver@gmail.com>
2022-10-10 19:06:42 +0200

    add: Navbar overlapping

--- client/src/Navigation/NavigationBar.tsx
+++ client/src/Navigation/NavigationBar.tsx
@@ -19,33 +19,17 @@ const NavBar = () => {
   const [open, setOpen] = useState(true);
 
   return (
-    <section>
+    <section className="w-full h-screen scrollbar-hide">
       {/* El header va aquí */}
-      <div className="flex justify-between order-last bg-white shadow-lg shadow-black-500/40 w-full ">
+      <div className="bg-white shadow-lg shadow-black-500/40 w-full ">
         <div className="py-3 px-3 font-extrabold italic text-fitpassGreen text-left text-2xl ">
           FitPass
         </div>
-        <div className="text-fitpassGreen py-3 px-3">
-          <AiOutlineMenu
-            size={26}
-            className={`order-last self-end cursor-pointer  ${
-              open ? "w-16" : "w-16"
-            }`}
-            onClick={() => setOpen(!open)}
-          />
-        </div>
-      </div>
-
-      <div className="flex justify-between gap-6">
-        <div className="m-3 text-x1 text-gray-900 font-semibold">
-          SCREEN OVERLAYED TEST
-          <img></img>
-        </div>
 
         <div
-          className={`bg-[#269FAE] min-h-screen ${
+          className={`absolute top-0 right-0 bg-[#269FAE] h-screen ${
             open ? "w-16" : "w-72"
-          } duration-300 justify-end text-white px-4 divide-y-2`}
+          } duration-300 text-white px-4 divide-y-2`}
         >
           <div className="py-3 flex justify-center">
             <AiOutlineMenu
@@ -54,7 +38,7 @@ const NavBar = () => {
               onClick={() => setOpen(!open)}
             />
           </div>
-          <div className="flex flex-col justify-between">
+          <div className="flex flex-col w-full h-screen">
             <div className="mt-4 flex flex-col gap-4 relative flex-1">
               {menus?.map((menu, i) => (
                 <>
@@ -79,7 +63,7 @@ const NavBar = () => {
 
             <div className="">
               <button
-                className={`border-2 border-white bg-white text-fitpassGreen font-bold
+                className={` mb-32 border-2 border-white bg-white text-fitpassGreen font-bold
               hover:bg-fitpassGreen hover:border-2 hover:text-white hover:border-white
                py-2 px-4 rounded-full whitespace-pre duration-500 ${
                  open && "opacity-0 translate-x-28 overflow-hidden"
@@ -90,6 +74,15 @@ const NavBar = () => {
             </div>
           </div>
         </div>
+      </div>
+
+      <div className="flex justify-between gap-6">
+        <div className="m-3 text-x1 text-gray-900 font-semibold">
+          SCREEN OVERLAYED TEST
+          <img></img>
+        </div>
+
+        
       </div>
     </section>
   );
