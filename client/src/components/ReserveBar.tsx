b0cbf3aa810bd7b4e8b0b8642c5631cd80f99eb7
militabu <buzaitemilita@gmail.com>
2022-10-14 20:30:33 +0200

    edited the text on reserve button

--- client/src/components/ReserveBar.tsx
+++ client/src/components/ReserveBar.tsx
@@ -18,7 +18,7 @@ export default function ReserveBar() {
   return (
     <footer className="flex justify-around py-3 border-t-[1px] shadow-[0_8px_10px_-7px_rgba(0,0,0,0.4)_inset]">
       <ReserveButton buttonText='BACK' backgroundColor='#fff' textColor='#000' hoverBackgroundColor="#269FAE" hoverTextColor="#fff" nav={() => navigate('/')} />
-      <ReserveButton buttonText={isAuthenticated ? 'LOGIN TO RESERVE' : 'RESERVE'} backgroundColor='#6F87F5' textColor='#FFF' hoverBackgroundColor="#269FAE" hoverTextColor="#fff" nav={handleReserve} />
+      <ReserveButton buttonText={isAuthenticated ? 'RESERVE' : 'RESERVE'} backgroundColor='#6F87F5' textColor='#FFF' hoverBackgroundColor="#269FAE" hoverTextColor="#fff" nav={handleReserve} />
     </footer>
   )
 }
