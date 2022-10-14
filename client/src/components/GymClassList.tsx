c83f71e82a55c35dc70eb3e3980afb65b94cf284
militabu <buzaitemilita@gmail.com>
2022-10-14 17:46:12 +0200

    fetched data from database to home

--- client/src/components/GymClassList.tsx
+++ client/src/components/GymClassList.tsx
@@ -1,18 +1,29 @@
-import React from 'react'
-import { GymClass } from '../mocks/GymClassMock';
+import React, { useEffect, useState } from 'react'
 import GymClassItem from './GymClassItem';
 import { Post } from '../../../globalTypes/Post';
+import { getGymClasses } from '../utils/api.service'
 
 export default function GymClassList() {
+
+  const [classes, setClasses] = useState([])
+
+  useEffect(() => {
+    getGymClasses()
+      .then(data => setClasses(data))
+      .catch(error => console.log(error))
+
+  }, [])
+
   return (
     <div className="flex flex-col w-[100%] h-[100%] gap-[1%] mt-[20%] md:mt-4">
-        {GymClass.map((post:Post) => {
+      {classes.map((post: Post) => {
         return (
-        <GymClassItem 
-          key={post.id} 
-          {...post}
-        />
-      )})}
+          <GymClassItem
+            key={post.id}
+            {...post}
+          />
+        )
+      })}
     </div>
   )
 }
