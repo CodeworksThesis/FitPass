c9b40dabf080db4549016ac89baa92c8a8d7f16a
Francis_ldn <fnhh04@gmail.com>
2022-10-14 09:52:03 +0200

    resolve conflicts

--- client/src/pages/Favorites.tsx
+++ client/src/pages/Favorites.tsx
@@ -3,25 +3,10 @@ import { useGymClass } from '../hooks/useGymClass'
 
 export default function Favorites() {
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-  const { favoriteGymClassDetails, bookedGymClassDetails } = useGymClass();
-
-  console.log(bookedGymClassDetails)
-=======
   const { favoriteGymClassDetails } = useGymClass();
-
-  console.log(favoriteGymClassDetails)
->>>>>>> backend2
-=======
-  const { favoriteGymClassDetails, bookedGymClassDetails } = useGymClass();
-
-  console.log(bookedGymClassDetails)
->>>>>>> backend-francis
-
   return (
     <div> 
-      {bookedGymClassDetails.map(gymclass => 
+      {favoriteGymClassDetails.map(gymclass => 
       <div key={gymclass.id}>
         {gymclass.desc}
       </div>)}
