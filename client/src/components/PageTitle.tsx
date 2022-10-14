b782a8cc2ee1e2cb25a45d6de9ebce36be8da571
militabu <buzaitemilita@gmail.com>
2022-10-14 16:25:06 +0200

    title for home page

--- client/src/components/PageTitle.tsx
+++ client/src/components/PageTitle.tsx
@@ -7,7 +7,7 @@ interface PageTitleProps {
 export default function PageTitle({title}: PageTitleProps) {
   return (
     <header>
-        <h1 className="font-bold italic text-2xl leading-10 text-center py-3 text-focus-in">
+        <h1 className="font-bold italic text-1xl leading-10 text-center py-3 text-focus-in">
           {title}
         </h1>
     </header>
