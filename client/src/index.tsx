e7c45887223de108ba9ba82bb9bdcc2aea7d1299
militabu <buzaitemilita@gmail.com>
2022-10-14 10:19:14 +0200

    logo

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -7,7 +7,7 @@ import './index.css';
 
 const domain = process.env.REACT_APP_AUTH0_DOMAIN ?? '';
 const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID ?? '';
-const audience = process.env.REACT_APP_AUTH0_AUDIENCE 
+const audience = process.env.REACT_APP_AUTH0_AUDIENCE
 
 
 
@@ -26,3 +26,45 @@ ReactDOM.render(
   document.getElementById('root'),
 );
 
+
+
+Favorites.findOneAndUpdate({ “favorited.userId”: "updatedFavorite.favorited[0].userId" },
+    { $pull: { "favorited.$.gymClassId" : { updatedFavorite.favorited[0].gymClassId } } } )
+
+
+
+
+
+
+        if (err) {
+            return res.status(404).json({ message: 'Error' });
+        }
+        return res.status(200).json({
+            success: true,
+            message: 'success'
+        });
+    }
+);
+
+
+try {
+  const updatedFavorite = await req.body
+  const updates = await Favorites.findOne({“favorited.userId”: updatedFavorite.favorited[0].userId})
+  if(!updates || Object.keys(updates).length ===0){
+  const updateCreated= await Favorites.create(updatedFavorite)
+  console.log(updateCreated)
+  res.status(201);
+  res.send(updateCreated);
+  } else{
+       updates.favorited.map(item => {
+      if(item.userId === updatedFavorite.favorited[0].userId) {
+      return item.gymClassId = [...item.gymClassId, ...updatedFavorite.favorited[0].gymClassId]}
+      return item })
+      updates.save()
+    res.send(updates)
+    res.status(201)
+  }
+} catch (e) {
+  console.log(e);
+  res.status(400).end();
+}
