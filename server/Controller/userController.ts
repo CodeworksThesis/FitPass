cf6f483a324c7b4923a885d17c35699eba76d5c0
militabu <buzaitemilita@gmail.com>
2022-10-18 18:30:22 +0200

    conflicts

--- server/Controller/userController.ts
+++ server/Controller/userController.ts
@@ -10,10 +10,6 @@ const axios = require('axios')
 const { cloudinary } = require('../utils/cloudinary')
 require('dotenv').config()
 
-const axios = require('axios')
-const { cloudinary } = require('../utils/cloudinary')
-require('dotenv').config()
-
 export const getFavorites = async (req: Request, res: Response) => {
   try {
     const { id } = req.params
